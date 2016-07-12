65c2071e3727044683d8f84f7b99367e435a6d01
GAylabea <gbea@rocketmail.com>
2016-07-11 20:07:57 -0500

    uppercase the user's name when telling them they are GRAND plus Notes!

--- Program.cs
+++ Program.cs
@@ -11,7 +11,13 @@ namespace Cheers
         {
             Console.WriteLine("What's your name?");
             Console.Write("> ");
-            String user_name = Console.ReadLine();
+            String user_name = Console.ReadLine(); //read from terminal to save a variable
+            
+            // user_naem.ToUpper() is turing the user's name to uppercase.
+            // ToUpper() is functionality specific to Strings
+            // ToUpper) accepts no arguments
+            
+            Console.WriteLine(user_name.ToUpper() + " is..GRAND!"); 
         }
     }
 }
